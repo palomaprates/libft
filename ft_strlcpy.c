b5fa8df6433bedb0b558218f4fd0130a97284495
paloma <paloma.prates108@gmail.com>
2024-04-20 09:20:07 +0100

    add strlcpy

---
+++ ft_strlcpy.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: paloma <paloma@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/18 21:06:50 by paloma            #+#    #+#             */
+/*   Updated: 2024/04/20 09:15:58 by paloma           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+size_t	ft_strlcpy(char *dest, const char  *src, size_t dest_size)
+{
+	size_t i;
+	size_t src_len;
+
+	i = 0;
+	src_len = 0;
+	while (src[i] && i < dest_size - 1)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (src[src_len])
+		src_len++;
+	return (src_len);
+}
+
+int	main()
+{
+	char dest[15] = "1234";
+	char src[40] = "6313123";
+	printf("%zu\n", ft_strlcpy(dest, src, 2));
+	printf("%zu\n", strlcpy(dest, src, 2));
+	printf("%s\n", dest);
+	return (0);
+}
