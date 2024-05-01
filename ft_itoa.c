4c492b3424d1fdb989df114f4a1c52de40dd89d4
Paloma Prates Dos Santos <pprates-@c1r21s7.42lisboa.com>
2024-05-01 20:31:53 +0100

    add funcs

---
+++ ft_itoa.c
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pprates- <pprates-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/01 18:33:58 by pprates-          #+#    #+#             */
+/*   Updated: 2024/05/01 20:28:05 by pprates-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+int	get_len(int n)
+{
+	int	i;
+
+	i = 0;
+	if (n < 0)
+	{
+		i++;
+		n *= -1;
+	}
+	while(n > 0)
+	{
+		n = n / 10;
+		i++;
+	}
+	return (i);
+}
+char	*ft_itoa(int n)
+{
+	char	*str;
+	int	len;
+
+	len = get_len(n);
+	str = malloc(sizeof(char) * len + 1);
+	if (!str)
+		return (NULL);
+	str[len] = '\0';
+	len--;
+	if (n < 0)
+	{
+		str[0] = '-';
+		n *= -1;
+	}
+ 	while (n > 0)
+	{
+		str[len] = n % 10 + 48;
+		n = n / 10;
+		len--;
+	}
+	return (str);
+}
+
+
+int main()
+{
+	printf("%s\n", ft_itoa(-154));
+	return (0);
+}
