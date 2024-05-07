b5f230930f1938f492f53f23b185c48685b73dbb
Paloma Prates Dos Santos <pprates-@c2r1s6.42lisboa.com>
2024-05-07 14:36:45 +0100

    split

---
+++ ft_split.c
@@ -0,0 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pprates- <pprates-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/01 17:55:51 by pprates-          #+#    #+#             */
+/*   Updated: 2024/05/07 14:35:06 by pprates-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include "libft.h"
+
+int	count_words(char const *s, char c)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (s[i])
+	{
+		if ((s[i + 1] == c && s[i] != c) 
+		|| (s[i + 1] == '\0' && s[i] != c))
+			j++;
+		i++;
+	}
+	return (j);
+}
+char	**ft_split(char const *s, char c)
+{
+	char	**new_str;
+	int	words_nbr;
+	int	i;
+	int	j;
+
+	words_nbr = count_words(s, c);
+	new_str = malloc(sizeof(char *) * (words_nbr + 1));
+	if (!new_str)
+		return (NULL);
+	j = 0;
+	i = 0;
+	while (j < words_nbr)
+	{
+		while (s[i] && s[i] == c)
+			i++;
+		s = s + i;
+		i = 0;
+		while (s[i] && s[i] != c)
+			i++;
+		new_str[j] = malloc(sizeof(char) * (i + 1));
+		if (!new_str[j])
+			return (NULL);
+		ft_strlcpy(new_str[j++], s, ++i);
+	}
+	return (new_str[j] = NULL, new_str);
+}
+
+int	main()
+{
+	char const s[] = "--teste----oi-a";
+	char	**new_str;
+	new_str = ft_split(s,'-');
+	while(*new_str)
+	{
+		printf("%s\n", *new_str);
+		new_str++;
+	}
+	return (0);
+}
